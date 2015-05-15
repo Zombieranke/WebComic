478fb3c2d63e11df427206d60063ff523def52a8
Zombieranke <abcdefghijklmnop@qrs>
2015-05-15 11:46:14 +0200

    Added html skeleton for the comic page and some pictures for the navigation

--- index.php
+++ index.php
@@ -1,5 +1,40 @@
-<?php
-	echo 'Hello World';
-	echo 'Ich teste mich jetzt selbst. -Gouki';
-	echo "Zombieranke ist OP";
-?>
+<!DOCTYPE html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="style.css">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+		<title>
+			W3BC0M1C
+		</title>
+	</head>
+	
+	<body>
+		<div id="wrapper">
+			<div id="logo">
+			</div>
+			<ul id="navigation">
+				<li><a>Comic</a> </li>
+				<li><a>Über mich</a>	</li>
+				<li><a>Merchandise</a>	</li>
+				<li><a>Kontakt</a>	</li>
+			</ul>
+			
+			<div id="content">
+				<ul class="comic_navigation">
+					<li><a href="index.php?ältester_Strip"> <img src="pictures/doubleArrowLeft.png" alt="Go to oldest strip"> </a>	</li>
+					<li><a href="index.php?voriger_Strip"> <img src="pictures/singleArrowLeft.png" alt="Go to previous strip"></a>	</li>
+					<li><a href="index.php?zufälliger_Strip"> <img src="pictures/doubleArrowLeft.png" alt="Go to random strip"></a>	</li>
+					<li><a href="index.php?nächster_Strip"> <img src="pictures/singleArrowRight.png" alt="Go to next strip"></a>	</li>
+					<li><a href="index.php?neuester_Strip"> <img src="pictures/doubleArrowRight.png" alt="Go to latest strip"></a>	</li>
+				</ul>
+				<img id="Comicbild"	>
+				<ul class="comic_navigation">
+					<li><a href="index.php?ältester_Strip"> <img src="pictures/doubleArrowLeft.png" alt="Go to oldest strip"> </a>	</li>
+					<li><a href="index.php?voriger_Strip"> <img src="pictures/singleArrowLeft.png" alt="Go to previous strip"></a>	</li>
+					<li><a href="index.php?zufälliger_Strip"> <img src="pictures/doubleArrowLeft.png" alt="Go to random strip"></a>	</li>
+					<li><a href="index.php?nächster_Strip"> <img src="pictures/singleArrowRight.png" alt="Go to next strip"></a>	</li>
+					<li><a href="index.php?neuester_Strip"> <img src="pictures/doubleArrowRight.png" alt="Go to latest strip"></a>	</li>
+				</ul>
+			</div>
+		</div>
+	</body>
+</html>
