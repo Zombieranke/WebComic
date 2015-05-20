5ab58e00f502072a6c841450e398981d07f72b85
Zelda Fan <zeldafan42@gmail.com>
2015-05-20 12:08:51 +0200

    Implemented delete functionality

---
+++ editStripsModule.php
@@ -0,0 +1,54 @@
+<?php
+	//For now delete only
+	require_once('authHandler.php');
+	require_once ('stripFunctions.php');
+	
+	if(isAuthorized(ADMIN))
+	{
+		echo "	<head>
+					<link type=\"text/css\" rel=\"stylesheet\" href=\"style.css\">
+					<meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/>
+					<title>
+						W3BC0M1C
+					</title>
+				</head>";
+		
+		if(isset($_POST['delete']))
+		{
+			if(is_numeric($_POST['delete']))
+			{
+				deleteStrip($_POST['delete']);
+			}
+			else
+			{
+				echo "<p>Ungültige Parameter</p>";
+			}
+		}
+		
+		$strips = getStrips();
+		echo "<h1> Edit your Strips </h1>";
+		foreach($strips as $strip)
+		{
+			echo "<div class=\"strip\">";
+			echo "<h2>".$strip['name']."</h2>";
+			echo "<img class=\"editThumbs\" src=\"".$strip['file']."\"/>";
+			echo "<p>Ver&ouml;ffentlichungsdatum: ".$strip['date']."</p>";
+			echo "<form action=\"".htmlspecialchars($_SERVER['PHP_SELF'])."\" method=\"POST\">";
+			echo "<button type=\"submit\" name=\"delete\" value=\"".$strip['id']."\">Delete this strip</button>";
+			echo "</form>";
+			echo "</div>";
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+?>
---
+++ stripFunctions.php
@@ -0,0 +1,243 @@
+<?php
+	if(!defined('includeConnDetails'))
+	{
+		define('includeConnDetails', TRUE);
+	}
+	
+	function fetchStrip($id)
+	{
+		require ('connDetails.php');
+		if(!is_numeric($id))
+		{
+			die("Parameter unzul&auml;ssig");
+		}
+	
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+	
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+		
+		$stmt = $connection->prepare("SELECT datei FROM strips WHERE strip_id= ? AND veröffentlichungsdatum < CURRENT_TIMESTAMP");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$stmt->bind_result($file);
+		$stmt->fetch();
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+	
+		return $file;
+	}
+	
+	function fetchStripAbs($id)
+	{
+		require ('connDetails.php');
+		if(!is_numeric($id))
+		{
+			die("Parameter unzul&auml;ssig");
+		}
+	
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+	
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+	
+		$stmt = $connection->prepare("SELECT datei FROM strips WHERE strip_id= ?");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$stmt->bind_result($file);
+		$stmt->fetch();
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+	
+		return $file;
+	}
+	
+	function getNextId($id)
+	{
+		require ('connDetails.php');
+		if(!is_numeric($id))
+		{
+			die("Parameter unzul&auml;ssig");
+		}
+	
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+	
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+	
+		$stmt = $connection->prepare("SELECT strip_id FROM strips WHERE veröffentlichungsdatum > (SELECT veröffentlichungsdatum FROM strips WHERE strip_id = ?) AND veröffentlichungsdatum < CURRENT_TIMESTAMP ORDER BY veröffentlichungsdatum ASC LIMIT 1");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$stmt->bind_result($resId);
+		$stmt->fetch();
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+	
+		return $resId;
+	}
+	
+	function getPreviousId($id)
+	{
+		require ('connDetails.php');
+		if(!is_numeric($id))
+		{
+			die("Parameter unzul&auml;ssig");
+		}
+	
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+	
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+	
+		$stmt = $connection->prepare("SELECT strip_id FROM strips WHERE veröffentlichungsdatum < (SELECT veröffentlichungsdatum FROM strips WHERE strip_id = ?) AND veröffentlichungsdatum < CURRENT_TIMESTAMP ORDER BY veröffentlichungsdatum DESC LIMIT 1");
+		$stmt->bind_param("i", $id);
+		$stmt->execute();
+		$stmt->bind_result($resId);
+		$stmt->fetch();
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+	
+		return $resId;
+	}
+	
+	function getFirstId()
+	{
+		require ('connDetails.php');
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+	
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+	
+		$stmt = $connection->prepare("SELECT strip_id FROM strips WHERE veröffentlichungsdatum < CURRENT_TIMESTAMP ORDER BY veröffentlichungsdatum ASC LIMIT 1");
+		$stmt->execute();
+		$stmt->bind_result($resId);
+		$stmt->fetch();
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+	
+		return $resId;
+	}
+	
+	function getLatestId()
+	{
+		require ('connDetails.php');
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+	
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+	
+		$stmt = $connection->prepare("SELECT strip_id FROM strips WHERE veröffentlichungsdatum < CURRENT_TIMESTAMP ORDER BY veröffentlichungsdatum DESC LIMIT 1");
+		$stmt->execute();
+		$stmt->bind_result($resId);
+		$stmt->fetch();
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+	
+		return $resId;
+	}
+	
+	function getRandomId()
+	{
+		require ('connDetails.php');
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+	
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+	
+		$stmt = $connection->prepare("SELECT strip_id FROM strips WHERE veröffentlichungsdatum < CURRENT_TIMESTAMP ORDER BY RAND() ASC LIMIT 1");
+		$stmt->execute();
+		$stmt->bind_result($resId);
+		$stmt->fetch();
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+	
+		return $resId;
+	}
+	
+	function getStrips()
+	{
+		require ('connDetails.php');
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+		
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+		
+		$stmt = $connection->prepare("SELECT strip_id, name, datei, veröffentlichungsdatum FROM strips ORDER BY veröffentlichungsdatum DESC");
+		$stmt->execute();
+		$stmt->bind_result($id,$name,$file,$date);
+		
+		$i=0;
+		
+		while($stmt->fetch())
+		{
+			$result[$i]['id'] = $id;
+			$result[$i]['name'] = $name;
+			$result[$i]['file'] = $file;
+			$result[$i]['date'] = $date;
+			$i++;
+		}
+		
+		$stmt->free_result();
+		$stmt->close();
+		$connection->close();
+		
+		return $result;
+	}
+	
+	function deleteStrip($id)
+	{
+		require ('connDetails.php');
+		
+		if(!is_numeric($id))
+		{
+			die("Parameter unzul&auml;ssig");
+		}
+		
+		$connection = new mysqli($database['dbServer'],$database['dbUser'],$database['dbPassword'],$database['dbName']);
+		
+		if($connection->errno != 0)
+		{
+			die("Database connection failed: ".$connection->connect_error);
+		}
+		
+		$file = fetchStripAbs($id);
+		if(!empty($file))
+		{
+			unlink($file);
+			
+			$stmt = $connection->prepare("DELETE FROM strips WHERE strip_id = ?");
+			$stmt->bind_param("i", $id);
+			$stmt->execute();
+			$stmt->free_result();
+			$stmt->close();
+			$connection->close();
+		}
+		else
+		{
+			echo "<p>Could not find strip to delete</p>";
+		}
+	}
+?>
