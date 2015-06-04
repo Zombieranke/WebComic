f415bfe8ed607c2c5ab0af35eb193d376bdb9ded
Zombieranke <abcdefghijklmnop@qrs>
2015-06-04 15:34:27 +0200

    Working on style of login.php

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 		<input type="text" name="username" placeholder="Username"/>
 		<div class="loginIcon"><img src="pictures/lock-icon.png" alt="Lock-Icon for the password"></div>
 		<input type="password" name="password" placeholder="Password"/>
-		<a href="index.php?reset">Forgot your password? Reset here.</a>
+		<a href="index.php?reset">Forgot your password?</a>
 		<input type="submit" name="login" value="Login" id="loginButton"/>		
 	</fieldset>
 </form>
