b16ca85bd7b30048eaedeec8724490bdb5cb9a56
Zombieranke <arthur.bouz@gmx.at>
2015-06-14 15:20:53 +0200

    moved logo to logos, trying to separate navigation

--- connDetails.php
+++ connDetails.php
@@ -1,15 +1,15 @@
 <?php
-	if(!defined('includeConnDetails'))
-	{
-		header('HTTP/1.0 403 Forbidden');
-		die("It is forbidden to access this page directly");
-	}
-	else
-	{
-		$database['dbServer'] = 'localhost';
-		$database['dbName'] = 'WebComic';
-		$database['dbUser'] = 'root';
-		$database['dbPassword'] = 'password';
-	}
+						if(!defined('includeConnDetails'))
+						{
+							header('HTTP/1.0 403 Forbidden');
+							die("It is forbidden to access this page directly");
+						}
+						else
+						{
+							$database['dbServer'] = 'localhost';
+							$database['dbName'] = 'WebComic';
+							$database['dbUser'] = 'root';
+							$database['dbPassword'] = 'password';
+						}
 	
-?>
+					?>
